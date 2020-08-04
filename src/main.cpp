52b74e7eee6c077001abf1a36071b50200f68065
robbiedeloose <robbiedeloose@users.noreply.github.com>
2020-08-04 16:40:04 +0200

    added test topics

--- src/main.cpp
+++ src/main.cpp
@@ -41,7 +41,7 @@ int sunriseHour = 9;
 int sunriseMinute = 0;
 int sunsetHour = 21;
 int sunsetMinute = 0;
-int duration = 20; // in minutes
+int duration = 1; // in minutes
 int waitRGB = duration * 60 * 1000 / 255 / 2;
 int waitWhite = duration * 60 * 1000 / 1024 / 2;
 boolean daylight = false;
@@ -186,6 +186,34 @@ void setRGB1() {
 
 // WIFI FUNCTIONS ///////////////////////////////////////////////////////////////////////////////
 
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      Serial.println("send wakeupmassege");
+      client.publish("outTopic", "hello world");
+      // ... and resubscribe
+      Serial.println("subscribe");
+      client.subscribe("homie/aquarium999/led");
+      client.subscribe("homie/aquarium999/color");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+    Serial.println("MQTT reconnect done");
+}
+
 void setup_wifi() {
 
   delay(10);
@@ -210,29 +238,7 @@ void setup_wifi() {
   Serial.println(WiFi.localIP());
 }
 
-void reconnect() {
-  // Loop until we're reconnected
-  while (!client.connected()) {
-    Serial.print("Attempting MQTT connection...");
-    // Create a random client ID
-    String clientId = "ESP8266Client-";
-    clientId += String(random(0xffff), HEX);
-    // Attempt to connect
-    if (client.connect(clientId.c_str())) {
-      Serial.println("connected");
-      // Once connected, publish an announcement...
-      client.publish("outTopic", "hello world");
-      // ... and resubscribe
-      client.subscribe("inTopic");
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
-      delay(5000);
-    }
-  }
-}
+
 
 // MQTT FUNCTIONS ///////////////////////////////////////////////////////////////////////////////
 
@@ -240,18 +246,21 @@ void callback(char* topic, byte* payload, unsigned int length) {
   Serial.print("Message arrived [");
   Serial.print(topic);
   Serial.print("] ");
+  char buf[20] = "";
   for (int i = 0; i < length; i++) {
     Serial.print((char)payload[i]);
+    buf[i] = (char)payload[i];
   }
+  buf[length] = '\0';
   Serial.println();
+  //Serial.println(buf);
 
-  // Switch on the LED if an 1 was received as first character
-  if ((char)payload[0] == '1') {
-    digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
-    // but actually the LED is on; this is because
-    // it is active low on the ESP-01)
-  } else {
-    digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
+  if(strcmp(topic, "homie/aquarium999/led") == 0){
+    int payloadAsInt = atoi ((char*)payload);
+    Serial.println("Topic1");
+  } 
+  else if (strcmp(topic, "aquarium/aquarium999/color") == 0) {
+    Serial.println("Topic2");
   }
 
 }
@@ -404,8 +413,8 @@ void setup() {
 		daylight = false;
 	}	
 	if (daylight) {
-		sunrise();
-  	setRGB1();
+		//sunrise();
+  	//setRGB1();
 	}
 
   
