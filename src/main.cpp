a4069d727f5895ab5c5643ac61232b007679eb39
robbiedeloose <robbiedeloose@users.noreply.github.com>
2020-08-20 12:58:01 +0200

    made the wifi connect non blocking

--- src/main.cpp
+++ src/main.cpp
@@ -19,6 +19,7 @@ const char* ssid = STASSID;
 const char* password = STAPSK;
 //const char* mqtt_server = "broker.mqtt-dashboard.com";
 IPAddress mqtt_server(192, 168, 10, 161);
+boolean noWifiMode = false;
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -272,17 +273,35 @@ void setup_wifi() {
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
 
+	int wifiTimeOut = 0;
   while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+		if (wifiTimeOut < 60){
+			delay(500);
+			Serial.print(".");
+			noWifiMode = false;
+			wifiTimeOut++;
+		}
+		else {
+			noWifiMode = true;
+			break;
+		}
   }
 
   randomSeed(micros());
 
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
+	if (noWifiMode == false) {
+		Serial.println("");
+  	Serial.println("WiFi connected");
+  	Serial.println("IP address: ");
+  	Serial.println(WiFi.localIP());
+	}
+	else {
+		Serial.println("");
+		Serial.print("Could not connect to ");
+		Serial.println(ssid);
+		Serial.println("Started without wifi");
+	}
+
 }
 
 // MQTT FUNCTIONS ///////////////////////////////////////////////////////////////////////////////
@@ -440,68 +459,69 @@ void setup() {
 
   // WIFI
   setup_wifi();
-  client.setServer(mqtt_server, 1883);
-  client.setCallback(callback);
+	if (noWifiMode == false) {
+		client.setServer(mqtt_server, 1883);
+		client.setCallback(callback);
 
-  /*
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
-    Serial.println("Connection Failed! Rebooting...");
-    delay(5000);
-    ESP.restart();
-  }*/
+		/*
+		WiFi.mode(WIFI_STA);
+		WiFi.begin(ssid, password);
+		while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+			Serial.println("Connection Failed! Rebooting...");
+			delay(5000);
+			ESP.restart();
+		}*/
 
-  // Port defaults to 8266
-  // ArduinoOTA.setPort(8266);
+		// Port defaults to 8266
+		// ArduinoOTA.setPort(8266);
 
-  // Hostname defaults to esp8266-[ChipID]
-	#ifdef HOSTNAME 
-	ArduinoOTA.setHostname(HOSTNAME);
-	#endif
-  // No authentication by default
-  // ArduinoOTA.setPassword("admin");
+		// Hostname defaults to esp8266-[ChipID]
+		#ifdef HOSTNAME 
+		ArduinoOTA.setHostname(HOSTNAME);
+		#endif
+		// No authentication by default
+		// ArduinoOTA.setPassword("admin");
 
-  // Password can be set with it's md5 value as well
-  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
-  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+		// Password can be set with it's md5 value as well
+		// MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+		// ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
 
-  ArduinoOTA.onStart([]() {
-    String type;
-    if (ArduinoOTA.getCommand() == U_FLASH) {
-      type = "sketch";
-    } else { // U_FS
-      type = "filesystem";
-    }
+		ArduinoOTA.onStart([]() {
+			String type;
+			if (ArduinoOTA.getCommand() == U_FLASH) {
+				type = "sketch";
+			} else { // U_FS
+				type = "filesystem";
+			}
 
-    // NOTE: if updating FS this would be the place to unmount FS using FS.end()
-    Serial.println("Start updating " + type);
-  });
-  ArduinoOTA.onEnd([]() {
-    Serial.println("\nEnd");
-  });
-  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-  });
-  ArduinoOTA.onError([](ota_error_t error) {
-    Serial.printf("Error[%u]: ", error);
-    if (error == OTA_AUTH_ERROR) {
-      Serial.println("Auth Failed");
-    } else if (error == OTA_BEGIN_ERROR) {
-      Serial.println("Begin Failed");
-    } else if (error == OTA_CONNECT_ERROR) {
-      Serial.println("Connect Failed");
-    } else if (error == OTA_RECEIVE_ERROR) {
-      Serial.println("Receive Failed");
-    } else if (error == OTA_END_ERROR) {
-      Serial.println("End Failed");
-    }
-  });
-  ArduinoOTA.begin();
-  Serial.println("Ready");
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-  
+			// NOTE: if updating FS this would be the place to unmount FS using FS.end()
+			Serial.println("Start updating " + type);
+		});
+		ArduinoOTA.onEnd([]() {
+			Serial.println("\nEnd");
+		});
+		ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+			Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+		});
+		ArduinoOTA.onError([](ota_error_t error) {
+			Serial.printf("Error[%u]: ", error);
+			if (error == OTA_AUTH_ERROR) {
+				Serial.println("Auth Failed");
+			} else if (error == OTA_BEGIN_ERROR) {
+				Serial.println("Begin Failed");
+			} else if (error == OTA_CONNECT_ERROR) {
+				Serial.println("Connect Failed");
+			} else if (error == OTA_RECEIVE_ERROR) {
+				Serial.println("Receive Failed");
+			} else if (error == OTA_END_ERROR) {
+				Serial.println("End Failed");
+			}
+		});
+		ArduinoOTA.begin();
+		Serial.println("Ready");
+		Serial.print("IP address: ");
+		Serial.println(WiFi.localIP());
+	}	
   // Led strips //
   pinMode(12, OUTPUT);
   pixels.begin();
@@ -587,11 +607,13 @@ void setup() {
 
 void loop() {
   
-  if (!client.connected()) {
-    reconnect();
-  }
-  client.loop();
-  ArduinoOTA.handle();
+	if (noWifiMode = false) {
+		if (!client.connected()) {
+			reconnect();
+		}
+		client.loop();
+		ArduinoOTA.handle();
+	}
 
   if (millis() > time_now + period) {
 		time_now = millis();
